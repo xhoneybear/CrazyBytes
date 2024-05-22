8c39ed8df40f06401f19c4548cd5fc121f0b4720
Camille Lisek <camille@lisek.dev>
2024-05-22 03:11:15 +0200

    Remove WIP server classes
    The server class files were not meant to be added ATM, especially
    the GameCode class, which is probably not going to be a part
    of the server module. Removed them for now.

--- app/src/main/java/dev/lisek/crazybytes/server/Server.java
+++
@@ -1,5 +0,0 @@
-package dev.lisek.crazybytes.server;
-
-public class Server {
-    
-}
--- app/src/main/java/dev/lisek/crazybytes/server/Signal.java
+++
@@ -1,7 +0,0 @@
-package dev.lisek.crazybytes.server;
-
-public class Signal {
-    // public final static String START;
-    // public final static String DRAW;
-    // public final static String PLAY;
-}
